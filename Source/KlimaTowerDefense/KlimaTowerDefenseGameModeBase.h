115097c30ee91d356b180db80e51256556eaa9dd
Marcin-Klima <mnklima95@gmail.com>
2020-08-04 21:21:18 +0200

    Initial commit

---
+++ Source/KlimaTowerDefense/KlimaTowerDefenseGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "KlimaTowerDefenseGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class KLIMATOWERDEFENSE_API AKlimaTowerDefenseGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
